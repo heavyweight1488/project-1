1f97358eefa01f8cc1bbed01dbae97d450c30445
Vladislav Petrashko <vlad.petrashko78@gmail.com>
2022-11-07 14:58:26 +0300

    hangman

---
+++ controlWork10.py
@@ -0,0 +1,46 @@
+import random
+
+
+def hangman():
+    words = ["яблоко", "груша", "ява",
+             "питон", "висилица", "кот", "собака"]
+    randomWords = random.choice(words)
+    word = randomWords
+    wrong = 0
+    stages = ["",
+              "________         ",
+              "|                ",
+              "|        |       ",
+              "|        0       ",
+              "|       /|\      ",
+              "|       / \      ",
+              "|                "
+              ]
+    
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Добро пожаловать на казнь!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Введите букву: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("Вы выиграли! Было загадано слово: ")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("Вы проиграли! Было загадано слово: {}.".format(word))
+        
+print(hangman())
